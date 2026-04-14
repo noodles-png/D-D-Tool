ec64a4594cee949e4aef26101bb583d32e69a4ec
Patrick Carillo <p.carillo17@googlemail.com>
2026-04-14 16:07:29 +0200

    Added feat: add spells table and API sync

--- sync_spells.py
+++ sync_spells.py
@@ -1,14 +1,19 @@
 from database.db_manager import DnDDatabase
+from api.dnd_api import get_all_spells, get_spell
 
 
-db = DnDDatabase()
+def sync_all_spells():
+    db = DnDDatabase()
+    indices = get_all_spells()
+    for i, api_index in enumerate(indices, start=1):
+        spell = get_spell(api_index)
+        spell_name = spell["name"]
+        spell_level = spell["level"]
+        spell_school = spell["school"]["name"]
+        description = "\n\n".join(spell["desc"])
+        db.add_spell(api_index, spell_name, spell_level, spell_school, description)
+        print(f"[{i}|{len(indices)}] {spell_name}")
+    db.close_connection()
 
-
-
-
-
-
-
-
-
-db.close_connection()
+if __name__ == "__main__":
+    sync_all_spells()
